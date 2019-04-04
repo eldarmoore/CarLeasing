fc44ce441811d2df1d8120f75b03d0790d7ee7b5
Eldar <eldar.moore@gmail.com>
2019-04-04 13:36:02 +0300

    Installing bootstrap with nav bar and starting with Angular template forms

--- CarLeasingApp-SPA/src/app/app.module.ts
+++ CarLeasingApp-SPA/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule, MatListModule} from '@angular/material';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
+      FormsModule,
       BrowserAnimationsModule,
-      MatButtonModule, MatCheckboxModule, MatListModule
+      MatButtonModule,
+      MatCheckboxModule,
+      MatListModule
    ],
    providers: [],
    bootstrap: [
