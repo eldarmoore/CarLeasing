5328f21f09c4f7da96df41db98eeb78dabb005af
Eldar <eldar.moore@gmail.com>
2019-04-04 14:03:36 +0300

    Injecting the Angular service in Component

--- CarLeasingApp-SPA/src/app/components/nav/nav.component.ts
+++ CarLeasingApp-SPA/src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,12 +10,16 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login() {
-    console.log(this.model);
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in successfully');
+    }, error => {
+      console.log('Failed to login');
+    });
   }
 }
--- CarLeasingApp-SPA/src/app/services/auth.service.ts
+++ CarLeasingApp-SPA/src/app/services/auth.service.ts
@@ -18,7 +18,7 @@ login(model: any) {
         localStorage.setItem('token', user.token);
       }
     })
-  )
+  );
 }
 
 }
