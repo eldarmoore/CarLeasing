30064d712b276e171a4fba6491df017837cb0a17
Eldar <eldar.moore@gmail.com>
2019-04-07 19:37:58 +0300

    Extending the User Model

--- CarLeasingApp.API/Models/User.cs
+++ CarLeasingApp.API/Models/User.cs
@@ -1,10 +1,18 @@
+using System;
+using System.Collections.Generic;
+
 namespace CarLeasingApp.API.Models
 {
     public class User
     {
         public int Id { get; set; }
+        public string FullName { get; set; }
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
