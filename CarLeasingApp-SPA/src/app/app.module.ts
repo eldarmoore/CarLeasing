fb7540e57fb5fdefafccec5ac8defeff5ffda9e9
Eldar <eldar.moore@gmail.com>
2019-04-07 14:12:39 +0300

    Setting up routing in Angular

--- CarLeasingApp-SPA/src/app/app.module.ts
+++ CarLeasingApp-SPA/src/app/app.module.ts
@@ -1,43 +1,68 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule, MatCheckboxModule, MatListModule, MatSnackBarModule, MatToolbarModule, MatIconModule, MatInputModule, MatMenuModule, MatFormFieldModule } from '@angular/material';
-import { MatCarouselModule } from '@ngmodule/material-carousel';
-import { MatCardModule } from '@angular/material/card';
-import { MatSelectModule } from '@angular/material/select';
-import { NavComponent } from './components/nav/nav.component';
-import { FormsModule } from '@angular/forms';
-import { AuthService } from './services/auth.service';
-import { HomeComponent } from './components/home/home.component';
-import { RegisterComponent } from './components/register/register.component';
-import { CarouselComponent } from './components/carousel/carousel.component';
-import { ErrorInterceptorProvider } from './services/error.interceptor';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatListModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatInputModule,
+  MatMenuModule,
+  MatFormFieldModule
+} from '@angular/material';
+import {MatCarouselModule} from '@ngmodule/material-carousel';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {NavComponent} from './components/nav/nav.component';
+import {FormsModule} from '@angular/forms';
+import {AuthService} from './services/auth.service';
+import {HomeComponent} from './components/home/home.component';
+import {RegisterComponent} from './components/register/register.component';
+import {CarouselComponent} from './components/carousel/carousel.component';
+import {ErrorInterceptorProvider} from './services/error.interceptor';
+import {RouterModule} from '@angular/router';
+import {appRoutes} from './routes';
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      NavComponent,
-      HomeComponent,
-      RegisterComponent,
-      CarouselComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule,
-      FormsModule,
-      BrowserAnimationsModule,
-      MatButtonModule, MatCheckboxModule, MatListModule, MatSnackBarModule, MatToolbarModule, MatIconModule, MatInputModule, MatMenuModule, MatFormFieldModule, MatSelectModule, MatCardModule, MatCarouselModule, MatDialogModule
-   ],
-   providers: [
-      AuthService,
-      ErrorInterceptorProvider
-   ],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    CarouselComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatListModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCardModule,
+    MatCarouselModule,
+    MatDialogModule
+  ],
+  providers: [
+    AuthService,
+    ErrorInterceptorProvider
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
